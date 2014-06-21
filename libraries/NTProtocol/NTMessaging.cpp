a5a201d2d515b1322c03c565a1d60ed584f33993
Wayne Keenan <wayne@shiny.local>
2014-06-21 06:28:16 +0100

    added NTProtocol lib

---
+++ libraries/NTProtocol/NTMessaging.cpp
@@ -0,0 +1,133 @@
+
+#include "NTMessaging.h"
+
+#ifdef ARDUINO
+#include <Arduino.h>
+#endif
+
+// share state
+NTQueue inboundMsgFIFO;
+NTQueue outboundMsgFIFO;
+
+// Shared functions, with no BLE device specifc implementation details
+
+
+void NT_sendCommand(int8_t cat, uint8_t cmd, uint8_t _id, uint16_t p1) {
+    uint8_t msg[NT_MSG_SIZE] = {
+        NT_DEFAULT_MSG_HEADER(),
+        NT_CREATE_CMD_NOP,//NT_CREATE_CMD(cat, cmd, NT_CMD_NO_CONT), _id, bytesFromInt(p1),
+        NT_CREATE_CMD_NOP,
+        NT_CREATE_CMD_NOP,
+        NT_CREATE_CMD_NOP
+    };
+#ifdef ARDUINO
+    Serial.print(cat, HEX);
+    Serial.print(" ");
+    Serial.print(cmd, HEX);
+    Serial.print(" ");
+    Serial.print(_id, HEX);
+    Serial.print(" ");
+    Serial.println(p1, HEX);
+#endif
+    NT_CREATE_CMD_WITH_ARG1((&msg[NT_MSG_HEADER_BYTES]), cat, cmd, _id, p1);
+    
+    /*
+     msg[NT_MSG_HEADER_BYTES]=NT_CREATE_CMD(cat, cmd, 0);
+     msg[NT_MSG_HEADER_BYTES+1]=_id;
+     msg[NT_MSG_HEADER_BYTES+2]=hiByteFromInt(p1);
+     msg[NT_MSG_HEADER_BYTES+3]=hiByteFromInt(p1);
+     */
+    NT_MSG_CALC_CRC(msg);
+    
+    NT_scheduleMsg(msg);
+}
+
+
+
+
+uint8_t  NT_scheduleMsg(uint8_t* buffer) {
+  BLEMessage* msg = new BLEMessage(buffer, NT_MSG_SIZE);
+  outboundMsgFIFO.enqueue(msg);
+    return 0;
+}
+
+void NT_processInboundMessageQueue() {
+
+  for (int i = 0; i < inboundMsgFIFO.count(); i++) {
+    BLEMessage* msg = inboundMsgFIFO.dequeue();
+    processMessage(msg->payload, msg->size);
+    delete msg;
+  }
+}
+
+
+
+void NT_processOutboundMessageQueue() {
+#ifdef ARDUINO
+    //Serial.print("Out FIFO COUNT");
+    //Serial.println(outboundMsgFIFO.count(), DEC);
+#endif
+    for (int i = 0; i < outboundMsgFIFO.count(); i++) {
+        BLEMessage* msg = outboundMsgFIFO.dequeue();
+#ifdef ARDUINO
+#ifdef USE_BLE
+
+#ifdef RFDUINO
+        RFduinoBLE.send((char*)msg->payload, NT_MSG_SIZE);
+#else // adafruit
+        BLESerial.write(msg->payload, NT_MSG_SIZE);
+#endif
+#else // UART
+        
+#ifdef ARDUINO_AVR_LEONARDO  // or a-star
+        //Serial.println("SENDING!");
+        Serial1.write(">>");
+        Serial1.write(msg->payload, NT_MSG_SIZE);
+#else
+        Serial.write(">>");
+        Serial.write(msg->payload, NT_MSG_SIZE);
+#endif // ARDUINO_AVR_LEONARDO
+        
+#endif // USE_BLE
+#else
+        // Non-arduiono send...
+        
+#endif // ARDUINO
+        
+        delete msg;
+    }
+}
+
+
+#define SERIAL_BUF_SIZE 32
+uint8_t serialBuffer[SERIAL_BUF_SIZE+1];
+int serialBufPtr = 0;
+bool foundStart = false;
+char c=0, lastChar=0;
+
+#ifdef ARDUINO
+void NT_UART_parseIntoQueue(HardwareSerial &ser, NTQueue &queue) {
+    
+    while (ser.available()>0) {
+        lastChar = c;
+        c =  ser.read();
+        //Serial.print(" ");
+        //Serial.print(c, HEX);
+        
+        if (foundStart && (serialBufPtr<SERIAL_BUF_SIZE)) {
+            serialBuffer[serialBufPtr++] = c;
+        } else if ( ('>' == c) && ('>' == lastChar) ) {
+            foundStart=true;
+            serialBufPtr=0;
+        }
+        
+        if (serialBufPtr>=NT_MSG_SIZE) {
+            //Serial.println("<EOC>");
+            foundStart = false;
+            BLEMessage* msg = new BLEMessage(serialBuffer, NT_MSG_SIZE);
+            queue.enqueue(msg);
+            serialBufPtr=0;
+        }
+    }
+}
+#endif
---
+++ libraries/NTProtocol/NTMessaging.h
@@ -0,0 +1,35 @@
+#ifndef NTMessaging_h
+#define NTMessaging_h
+
+
+#include <NTProtocol.h>
+#include <SimpleFIFO.h>
+#include <string.h>
+// Inbound & outbound FIFO's
+class BLEMessage {
+  public:
+    BLEMessage(uint8_t* buffer, uint16_t len) {
+      memcpy(payload, buffer, NT_MSG_SIZE);
+      size=len;
+    };
+  uint8_t payload[NT_MSG_SIZE];
+  uint8_t size;
+};
+
+typedef SimpleFIFO<BLEMessage*, 4> NTQueue;
+
+extern SimpleFIFO<BLEMessage*, 4> inboundMsgFIFO;
+extern SimpleFIFO<BLEMessage*, 4> outboundMsgFIFO;  
+
+
+uint8_t  NT_scheduleMsg(uint8_t* buffer);
+void     NT_processInboundMessageQueue();
+void     NT_processOutboundMessageQueue();
+
+#ifdef ARDUINO
+#include <Arduino.h>
+void NT_UART_parseIntoQueue(HardwareSerial &ser, NTQueue &queue);
+#endif
+
+#endif
+
