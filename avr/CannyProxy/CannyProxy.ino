bdc374829f376815a73102531ad5918d25b42860
Wayne Keenan <wayne@cannybots.com>
2014-08-12 13:54:49 +0100

    fixed gzll/ble radio toggling

--- avr/CannyProxy/CannyProxy.ino
+++ avr/CannyProxy/CannyProxy.ino
@@ -19,7 +19,7 @@
 
 #define BLE_UUID                 "7e400001-b5a3-f393-e0a9-e50e24dcca9e"
 #define BLE_LOCALNAME            "Cannybots"
-#define BLE_ADVERTISEMENT_DATA   "CB_LFB_001"   
+#define BLE_ADVERTISEMENT_DATA   "CB_LFB_001"
 // Note: BLE_ADVERTISEMENT_DATA must be < 16 bytes.
 
 #define BLE_TX_POWER_LEVEL  0
@@ -30,7 +30,7 @@
 
 // TODO: append devideID to advertising data
 
-#define TOGGLE_MILLIS 1500
+#define TOGGLE_MILLIS 1000
 
 #define Q_MAX_ENTRIES 12
 #define Q_ENTRY_SIZE 20
@@ -48,7 +48,7 @@
 #define RX_PIN 6
 // Gold board
 //#define TX_PIN 1
-//#define RX_PIN 0 
+//#define RX_PIN 0
 #endif
 
 
@@ -72,37 +72,37 @@ int  radio2uartQItems = 0;
 
 
 void enqueue(char *data, int len) {
-   if (radio2uartQItems == Q_MAX_ENTRIES)
-     return;
-    memcpy(radio2uartQ[radio2uartQTail++], data, len <= Q_ENTRY_SIZE ? len : Q_ENTRY_SIZE);
-    radio2uartQItems++;
-    if (radio2uartQTail == Q_MAX_ENTRIES) {
-      radio2uartQTail=0;
-    }
+  if (radio2uartQItems == Q_MAX_ENTRIES)
+    return;
+  memcpy(radio2uartQ[radio2uartQTail++], data, len <= Q_ENTRY_SIZE ? len : Q_ENTRY_SIZE);
+  radio2uartQItems++;
+  if (radio2uartQTail == Q_MAX_ENTRIES) {
+    radio2uartQTail = 0;
+  }
 }
 
 /////////////////////////////////////////////////////////
 // Serial
 
-uint8_t serialBuffer[SERIAL_BUF_SIZE+1];
+uint8_t serialBuffer[SERIAL_BUF_SIZE + 1];
 int serialBufPtr = 0;
 bool foundStart = false;
-char c=0, lastChar=0;
+char c = 0, lastChar = 0;
 
 
-void writeUART(const uint8_t* data, uint16_t len) {  
-  
+void writeUART(const uint8_t* data, uint16_t len) {
+
 
 #ifdef USE_SPI
-   digitalWrite(SS, LOW);    // SS is pin 10
-   SPI.transfer ('>');
-   SPI.transfer ('>');
-   for (int i = 0 ; i <len ; i++) {
-        DBG("SPI:%x",data[i]);
+  digitalWrite(SS, LOW);    // SS is pin 10
+  SPI.transfer ('>');
+  SPI.transfer ('>');
+  for (int i = 0 ; i < len ; i++) {
+    DBG("SPI:%x", data[i]);
 
-        SPI.transfer (data[i]);
-   }
-   digitalWrite(SS, HIGH);
+    SPI.transfer (data[i]);
+  }
+  digitalWrite(SS, HIGH);
 
 #else
   Serial.write(">>");
@@ -112,22 +112,22 @@ void writeUART(const uint8_t* data, uint16_t len) {
 }
 
 void process_uart2ble_q() {
-  while (UART_SOURCE.available()>0) {
-        lastChar = c;
-        c =  UART_SOURCE.read();
-        if (foundStart && (serialBufPtr<SERIAL_BUF_SIZE)) {
-            serialBuffer[serialBufPtr++] = c;
-        } else if ( ('>' == c) && ('>' == lastChar) ) {
-            foundStart=true;
-            serialBufPtr=0;
-        }
-        if (serialBufPtr>=CB_MSG_SIZE) {
-            foundStart = false;
-            // TODO: check for ID message (use it to set BLE adv data and also turn off ISP mode)
-            RFduinoBLE.send((const char*)serialBuffer, CB_MSG_SIZE);
-            serialBufPtr=0;
-        }
+  while (UART_SOURCE.available() > 0) {
+    lastChar = c;
+    c =  UART_SOURCE.read();
+    if (foundStart && (serialBufPtr < SERIAL_BUF_SIZE)) {
+      serialBuffer[serialBufPtr++] = c;
+    } else if ( ('>' == c) && ('>' == lastChar) ) {
+      foundStart = true;
+      serialBufPtr = 0;
     }
+    if (serialBufPtr >= CB_MSG_SIZE) {
+      foundStart = false;
+      // TODO: check for ID message (use it to set BLE adv data and also turn off ISP mode)
+      RFduinoBLE.send((const char*)serialBuffer, CB_MSG_SIZE);
+      serialBufPtr = 0;
+    }
+  }
 }
 
 /////////////////////////////////////////////////////////
@@ -139,18 +139,18 @@ boolean volatile gzll_connected = false;
 // GZLL
 volatile bool ISPConnected = false;
 
-volatile static unsigned long lastGZLLPacketTime = 0;
+volatile static unsigned long GZLLPacketTimeout = 0;
 
 void RFduinoGZLL_onReceive(device_t device, int rssi, char *data, int len)
 {
   gzll_connected = true;
-  lastGZLLPacketTime = millis();
+  GZLLPacketTimeout = millis() + GZLL_TIMEOUT;
   if (DEVICE1 == device ) {
-     ISPConnected = true;
+    ISPConnected = true;
   }
-  
+
   if (ISPConnected ) {
-     rfd_isp_RFduinoGZLL_onReceive(device, rssi, data, len);
+    rfd_isp_RFduinoGZLL_onReceive(device, rssi, data, len);
   } else {
     enqueue(data, len);
   }
@@ -162,20 +162,20 @@ void RFduinoGZLL_onReceive(device_t device, int rssi, char *data, int len)
 
 // BLE
 void RFduinoBLE_onAdvertisement(bool start) {
-    DBG("RFd_BLE_A=%d",start);
+  DBG("RFd_BLE_A=%d", start);
 }
 
 void RFduinoBLE_onConnect() {
   ble_connected = true;
-    DBG("RFd_BLE_CON",0);
+  DBG("RFd_BLE_CON", 0);
 }
 
 void RFduinoBLE_onDisconnect() {
   ble_connected = false;
-  DBG("RFd_BLE_DIS",0);
+  DBG("RFd_BLE_DIS", 0);
 }
 
-void RFduinoBLE_onReceive(char *data, int len) {  
+void RFduinoBLE_onReceive(char *data, int len) {
   enqueue(data, len);
 }
 
@@ -187,54 +187,82 @@ volatile unsigned long lastRadioToggleTime = millis();
 
 void  ble_rfduino_manageRadios() {
   unsigned long timeNow   = millis();
+  //DBG("time=%ld", timeNow);
 
-  if ( (timeNow - lastGZLLPacketTime ) > GZLL_TIMEOUT ) {
-      if (gzll_connected) {
-        //gzll_connected = ISPConnected = false;            
-        //state=0;
-      }
+  // have to use ABS because of the asyncronous delivery of radio packets and unsigned longs
+  // e.g. the GZLL packet may arrive 'sooner' that the value of 'timeNow'
+  if ( timeNow  > GZLLPacketTimeout ) {
+    gzll_connected = ISPConnected = false;
+    //DBG("GZTN=%ld", timeNow);
+    //DBG("GZTO=%ld", GZLLPacketTimeout);
   }
 
   if (ble_connected) {
-   return;
-  }
-  
- if (gzll_connected) {
     return;
   }
+  if (gzll_connected) {
+    return;
+  }
+  //DBG("SBG=(%d,%d,%d)", state, ble_connected, gzll_connected);
 
+
+  // Simple FSM:
+  switch (state) {
+    case 0:
+      DBG("RFd_RDIO_OFF", 0);
+
+      RFduinoGZLL.end();
+      RFduinoBLE.end();
+      state=1;
+      break;
+
+    case 1:    // NOP - 'wait' state whilst BLE is active, will remain here whilst BLE/GZLL connected or <TOGGLE_MILLIS
+      //DBG("RFd_NORDIO", 0);
+      break;  
+
+    case 2:
+      DBG("RFd_BLE_UP", 0);
+
+      RFduinoBLE.customUUID = BLE_UUID;
+      RFduinoBLE.deviceName = BLE_LOCALNAME;
+      RFduinoBLE.advertisementData = BLE_ADVERTISEMENT_DATA;
+      //RFduinoBLE.advertisementInterval(millis);
+
+      RFduinoBLE.txPowerLevel = BLE_TX_POWER_LEVEL;
+      RFduinoBLE.begin();
+      state=3;
+      break;
+      
+    case 3:    // NOP - 'wait' state whilst BLE is active,  will remain here whilst BLE/GZLL is connected or <TOGGLE_MILLIS
+      //DBG("RFd_BLE_ON", 0);
+      break;  
+      
+    case 4:
+      DBG("RFd_GZL_UP", 0);
+      while (RFduinoBLE.radioActive);
+      RFduinoBLE.end();
+      while (RFduinoBLE.radioActive);
+      
+      RFduinoGZLL.txPowerLevel = GZLL_TX_POWER_LEVEL;
+      RFduinoGZLL.begin(HOST);
+      state=5;
+
+    case 5:    // NOP - 'wait' state whilst BLE is active,  will remain here whilst BLE/GZLL is connected or <TOGGLE_MILLIS
+      //DBG("RFd_GZL_ON", 0);
+      break;           
+          
+    default:
+      //DBG("RFd_GZL_?%d", state);
+      state = 0;    // go back to the start state.
+      break;
+  }
+
+  // calculate next state  
   if ( (timeNow - lastRadioToggleTime) > TOGGLE_MILLIS) {
-    state++;
     lastRadioToggleTime =  timeNow;
+    state++;
+    //DBG("NewState=%d", state);
   }
-
-  if ( 0 == state) {
-    state = 1;
-
-    RFduinoGZLL.end();
-    RFduinoBLE.customUUID = BLE_UUID;
-    RFduinoBLE.deviceName = BLE_LOCALNAME;
-    RFduinoBLE.advertisementData = BLE_ADVERTISEMENT_DATA;
-    //RFduinoBLE.advertisementInterval(millis);
-    
-    RFduinoBLE.txPowerLevel= BLE_TX_POWER_LEVEL;
-    RFduinoBLE.begin();
-    DBG("RFd_BLE_ON",0);
-
-
-  } else if (2 == state  ) {
-    state = 3;
-    //while (!RFduinoBLE.radioActive);
-    while (RFduinoBLE.radioActive);
-    RFduinoBLE.end();
-    while (RFduinoBLE.radioActive);
-    RFduinoGZLL.txPowerLevel = GZLL_TX_POWER_LEVEL;
-    RFduinoGZLL.begin(HOST);
-    DBG("RFd_GZL_ON",0);
-  } else if (4 == state) {
-    state = 0;
-  }
-
 }
 
 
@@ -243,13 +271,13 @@ void  ble_rfduino_manageRadios() {
 // Queue processing
 
 void process_ble2uart_q() {
-  if (radio2uartQItems>0) { 
+  if (radio2uartQItems > 0) {
     writeUART((uint8_t*) radio2uartQ[radio2uartQHead++], Q_ENTRY_SIZE);
     radio2uartQItems--;
     if (radio2uartQHead == Q_MAX_ENTRIES) {
-      radio2uartQHead=0;
+      radio2uartQHead = 0;
     }
-  }  
+  }
 }
 
 
@@ -258,7 +286,7 @@ void process_ble2uart_q() {
 
 void setup() {
   Serial.begin(9600, RX_PIN, TX_PIN);        // UART Baud is limited to 9600 when the BLE stack is on.
-  
+
 #ifdef USE_SPI
 
   // Put SCK, MOSI, SS pins into output mode
@@ -267,7 +295,7 @@ void setup() {
   pinMode(SCK, OUTPUT);
   pinMode(MISO, INPUT);
   pinMode(MOSI, OUTPUT);
-  pinMode(SS, OUTPUT);  
+  pinMode(SS, OUTPUT);
   digitalWrite(SS, HIGH);  // ensure SS stays high for now
 
 
@@ -280,17 +308,14 @@ void setup() {
 
 #endif // USE_SPI
 
-
   rfd_isp_setup();
-  delay(2000);
   WATCHDOG_SETUP(7);
-  DBG("",0);
-  DBG("RFd_Setup",0);
+  DBG("RFd_UP!", 0);
 }
 
-void loop() {  
-   WATCHDOG_RELOAD();
-   //DBG("b,g,I=(%d,%d,%d)",ble_connected, gzll_connected, ISPConnected);
+void loop() {
+  WATCHDOG_RELOAD();
+  //DBG("b,g,I=(%d,%d,%d)",ble_connected, gzll_connected, ISPConnected);
 
   ble_rfduino_manageRadios();
 
