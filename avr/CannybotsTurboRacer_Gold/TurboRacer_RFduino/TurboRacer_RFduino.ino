2c21f3c7137fa90a3e8eecb003a0ce62f93ab6eb
Wayne Keenan <wayne.keenan@gmail.com>
2014-09-23 18:32:54 +0100

    re-added code from prior checkin

--- avr/CannybotsTurboRacer_Gold/TurboRacer_RFduino/TurboRacer_RFduino.ino
+++ avr/CannybotsTurboRacer_Gold/TurboRacer_RFduino/TurboRacer_RFduino.ino
@@ -9,6 +9,185 @@
 // Version:   1.0  -  22.09.2014  -  Inital Version  (Wayne Keenan & Anish Mampetta)
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////
+// uncomment this to get debug message on serial 
+// #define DEBUG
+// Note that it will interfer with the operation of the motor driver, still useful though.
+
+#include <RFduinoGZLL.h>
+#include <RFduinoBLE.h>
+
+// Bot constants
+
+////// Hardware Constants
+
+// Infrared
+#define IR_NUM_SENSORS               3
+
+#define IR1_PIN                      6
+#define IR2_PIN                      5
+#define IR3_PIN                      4
+
+// Motor Pins
+#define MOTOR_A1_PIN                 0
+#define MOTOR_A2_PIN                 3
+#define MOTOR_B1_PIN                 1
+#define MOTOR_B2_PIN                 2
+
+
+////// Processing constants
+
+#define IR1_BIAS                     0
+#define IR2_BIAS                     0
+#define IR3_BIAS                     0
+#define IR_WHITE_THRESHOLD         750
+
+#define MOTOR_MAX_SPEED            255
+#define MOTOR_CRUISE_SPEED         120
+
+#define OFF_LINE_MAX_TIME            0
+
+#define PID_P 25
+#define PID_D 250
+#define PID_SAMPLE_TIME              5
+
+
+#define JOYPAD_ID 0
+#define JOYPAD_AXIS_DEADZONE 10
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/// Bot Variables
+
+////// Inputs
+
+// IR Sensors
+int IRvals[IR_NUM_SENSORS] = {0};
+
+// Joypad
+volatile int16_t  xAxisValue    = 0;              // (left) -255 .. 255 (right)
+volatile int16_t  yAxisValue    = 0;              // (down) -255 .. 255 (up)
+volatile bool     buttonPressed = 0;              // 0 = not pressed, 1 = pressed
+
+
+////// Process / Algorithms
+
+// PID
+int Kp = PID_P;
+int Ki = 0;
+int Kd = PID_D;
+int P_error = 0;
+int I_sum = 0;
+int I_error = 0;
+int D_error = 0;
+int error = 0;
+int error_last = 0;                                 // to calculate D_error = error - error_last
+int correction = 0;
+
+
+// LineFollowing State
+bool isLineFollowingMode = false;
+bool forceManualMode     = false;
+
+// Timers in milli-seconds (1/1000 of a second)
+
+unsigned long timeNow = millis();                    // the time at the start of the loop()
+unsigned long pidLastTime = millis();                // when the PID was calculated last
+unsigned long offLineLastTime = millis();            // last time the bot came off the line
+unsigned long offTheLineTime = 0;                    // how long has the bot been off the line, total since last leaving the line
+
+////// Outputs
+
+// speeds are  -255 (fullspeed back) ..  255 (full speed forward)
+
+int cruiseSpeed = MOTOR_CRUISE_SPEED;      // default cruise speed when line following
+
+// The current requested/calculated motor speeds
+int speedA = 0;             // viewed from behind motor 'A' is on the left
+int speedB = 0;             // viewed from behind motor 'B' is on the right
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Arduino functions
+
+void setup() {
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
+
+  //  dumpAnalogReadingsForAllPins();
+
+  // Motor pins
+  pinMode(MOTOR_A1_PIN, OUTPUT);
+  pinMode(MOTOR_A2_PIN, OUTPUT);
+  pinMode(MOTOR_B1_PIN, OUTPUT);
+  pinMode(MOTOR_B2_PIN, OUTPUT);
+
+  //motorTest();
+
+  radio_setup();
+}
+
+void loop() {
+  radio_loop();
+
+  timeNow = millis();
+
+  readIRSensors();
+  updateLineFollowingStatus();
+
+  if (isLineFollowingMode) {
+    calculatePID();
+    joypadLineFollowingControlMode();
+  } else {
+    // in manual mode
+    joypadManualControlMode();
+    I_sum = 0; //set integral sum to zero
+  }
+  motorSpeed(speedA, speedB);
+#ifdef DEBUG
+  printVals();
+#endif
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// PID
+
+void calculatePID() {
+  // Calculate PID on a regular time basis
+  if ((timeNow - pidLastTime) < PID_SAMPLE_TIME ) {
+    // return if called too soon
+    return;
+  }
+  pidLastTime = timeNow;
+  // process IR readings via PID
+  Kp = PID_P;
+  Ki = 0;
+  Kd = PID_D;
+  error_last = error; // store previous error value before new one is caluclated
+  error = IRvals[0] - IRvals[2];
+  P_error = error * Kp / 100.0; // calculate proportional term
+  I_sum = constrain (I_sum + error, -1000, 1000); // integral term
+  I_error = I_sum * Ki / 100.0;
+  D_error = (error - error_last) * Kd / 100.0;          // calculate differential term
+  correction = P_error + D_error + I_error;
+  speedA = cruiseSpeed - correction;
+  speedB = cruiseSpeed + correction;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Joypad Handling
+
+// This is called when the bot is on the line
+// speedA and speedB will have already been set by pid_calculate() before this is run
+void  joypadLineFollowingControlMode() {
+  speedA = speedA + ((yAxisValue - (xAxisValue / 2)) / 2.0); //superpose yAxis with PID output speed
+  speedB = speedB + ((yAxisValue + (xAxisValue / 2)) / 2.0);
+}
+
+// This is called when the bot is in manual mode.
+// Use it to map the joypad X & Y axis (which both range from -255..255) to motor speeds (also -255..255)
+
 
 void joypadManualControlMode() {
 
